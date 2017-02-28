6d6433b301028092edff8f69fea0fa07f1ea3bbb
unknown <ys4013@eews506a-024.ic.ac.uk>
2017-02-28 21:02:20 +0000

    Q3

--- RF_2017/internal/getData.m
+++ RF_2017/internal/getData.m
@@ -2,81 +2,15 @@ function [ data_train, data_query ] = getData( MODE )
 % Generate training and testing data
 
 % Data Options:
-%   1. Toy_Gaussian
-%   2. Toy_Spiral
-%   3. Toy_Circle
 %   4. Caltech 101
-
+data_train = [];
+data_query = [];
 showImg = 1; % Show training & testing images and their image feature vector (histogram representation)
 
 PHOW_Sizes = [4 8 10]; % Multi-resolution, these values determine the scale of each layer.
 PHOW_Step = 8; % The lower the denser. Select from {2,4,8,16}
 
 switch MODE
-    case 'Toy_Gaussian' % Gaussian distributed 2D points
-        %rand('state', 0);
-        %randn('state', 0);
-        N= 150;
-        D= 2;
-        
-        cov1 = randi(4);
-        cov2 = randi(4);
-        cov3 = randi(4);
-        
-        X1 = mgd(N, D, [randi(4)-1 randi(4)-1], [cov1 0;0 cov1]);
-        X2 = mgd(N, D, [randi(4)-1 randi(4)-1], [cov2 0;0 cov2]);
-        X3 = mgd(N, D, [randi(4)-1 randi(4)-1], [cov3 0;0 cov3]);
-        
-        X= real([X1; X2; X3]);
-        X= bsxfun(@rdivide, bsxfun(@minus, X, mean(X)), var(X));
-        Y= [ones(N, 1); ones(N, 1)*2; ones(N, 1)*3];
-        
-        data_train = [X Y];
-        
-    case 'Toy_Spiral' % Spiral (from Karpathy's matlab toolbox)
-        
-        N= 50;
-        t = linspace(0.5, 2*pi, N);
-        x = t.*cos(t);
-        y = t.*sin(t);
-        
-        t = linspace(0.5, 2*pi, N);
-        x2 = t.*cos(t+2);
-        y2 = t.*sin(t+2);
-        
-        t = linspace(0.5, 2*pi, N);
-        x3 = t.*cos(t+4);
-        y3 = t.*sin(t+4);
-        
-        X= [[x' y']; [x2' y2']; [x3' y3']];
-        X= bsxfun(@rdivide, bsxfun(@minus, X, mean(X)), var(X));
-        Y= [ones(N, 1); ones(N, 1)*2; ones(N, 1)*3];
-        
-        data_train = [X Y];
-        
-    case 'Toy_Circle' % Circle
-        
-        N= 50;
-        t = linspace(0, 2*pi, N);
-        r = 0.4
-        x = r*cos(t);
-        y = r*sin(t);
-        
-        r = 0.8
-        t = linspace(0, 2*pi, N);
-        x2 = r*cos(t);
-        y2 = r*sin(t);
-        
-        r = 1.2;
-        t = linspace(0, 2*pi, N);
-        x3 = r*cos(t);
-        y3 = r*sin(t);
-        
-        X= [[x' y']; [x2' y2']; [x3' y3']];
-        Y= [ones(N, 1); ones(N, 1)*2; ones(N, 1)*3];
-        
-        data_train = [X Y];
-        
     case 'Caltech' % Caltech dataset
         close all;
         imgSel = [15 15]; % randomly select 15 images each class without replacement. (For both training & testing)
@@ -91,7 +25,7 @@ switch MODE
             figure('Units','normalized','Position',[.05 .1 .4 .9]);
             suptitle('Training image samples');
         end
-        for c = 1:length(classList)
+        for c = 1:length(classList) %1 to 10 classes
             subFolderName = fullfile(folderName,classList{c});
             imgList = dir(fullfile(subFolderName,'*.jpg'));
             imgIdx{c} = randperm(length(imgList));
@@ -102,7 +36,7 @@ switch MODE
                 I = imread(fullfile(subFolderName,imgList(imgIdx_tr(i)).name));
                 
                 % Visualise
-                if i < 6 & showImg
+                if i < 6 & showImg %five columns of picture
                     subaxis(length(classList),5,cnt,'SpacingVert',0,'MR',0);
                     imshow(I);
                     cnt = cnt+1;
@@ -115,37 +49,44 @@ switch MODE
                 
                 % For details of image description, see http://www.vlfeat.org/matlab/vl_phow.html
                 [~, desc_tr{c,i}] = vl_phow(single(I),'Sizes',PHOW_Sizes,'Step',PHOW_Step); %  extracts PHOW features (multi-scaled Dense SIFT)
-            end
+                   
+%                 bag = bagOfFeatures(I,'Verbose',false);
+            end     
         end
         
         disp('Building visual codebook...')
         % Build visual vocabulary (codebook) for 'Bag-of-Words method'
         desc_sel = single(vl_colsubset(cat(2,desc_tr{:}), 10e4)); % Randomly select 100k SIFT descriptors for clustering
+        %desc_sel is a 128x100k (nxp data) (observation x variables)
         
         % K-means clustering
         numBins = 256; % for instance,
         
-        
+        %Training data: vectors are histograms, one from each training image
         % write your own codes here
         % ...
-            
-       
+        cluster_centers = kmeans(desc_sel,numBins);
+
+        
         disp('Encoding Images...')
         % Vector Quantisation
         
         % write your own codes here
         % ...
-  
+        desc_tr = single(vl_colsubset(cat(2,desc_tr{:}), 10e4));
+
+        
         
         % Clear unused varibles to save memory
         clearvars desc_tr desc_sel
 end
 
+
 switch MODE
     case 'Caltech'
         if showImg
-        figure('Units','normalized','Position',[.05 .1 .4 .9]);
-        suptitle('Testing image samples');
+            figure('Units','normalized','Position',[.05 .1 .4 .9]);
+            suptitle('Testing image samples');
         end
         disp('Processing testing images...');
         cnt = 1;
@@ -170,27 +111,34 @@ switch MODE
                     I = rgb2gray(I);
                 end
                 [~, desc_te{c,i}] = vl_phow(single(I),'Sizes',PHOW_Sizes,'Step',PHOW_Step);
-            
+                 %desc_sel1{c,i} 
+                 temp= single(vl_colsubset(cat(2,desc_te{c,i}), 2000)); % Randomly select 100k SIFT descriptors for clustering
+        
+                
             end
         end
-       % suptitle('Testing image samples');
-                if showImg
+        %        suptitle('Testing image samples');
+        if showImg
             figure('Units','normalized','Position',[.5 .1 .4 .9]);
-        suptitle('Testing image representations: 256-D histograms');
+            suptitle('Testing image representations: 256-D histograms');
         end
-
+        
         % Quantisation
         
         % write your own codes here
         % ...
         
         
-    otherwise % Dense point for 2D toy data
-        xrange = [-1.5 1.5];
-        yrange = [-1.5 1.5];
-        inc = 0.02;
-        [x, y] = meshgrid(xrange(1):inc:xrange(2), yrange(1):inc:yrange(2));
-        data_query = [x(:) y(:) zeros(length(x)^2,1)];
-end
+        
+
+        
+        
+        %     otherwise % Dense point for 2D toy data
+        %         xrange = [-1.5 1.5];
+        %         yrange = [-1.5 1.5];
+        %         inc = 0.02;
+        %         [x, y] = meshgrid(xrange(1):inc:xrange(2), yrange(1):inc:yrange(2));
+        %         data_query = [x(:) y(:) zeros(length(x)^2,1)];
+        % end
 end
 
