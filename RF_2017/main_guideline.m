5c76b78ada7d39db1b27daa6161ef0ee5f1bdd35
unknown <ys4013@eews506a-013.ic.ac.uk>
2017-03-07 14:12:10 +0000

    add time

--- RF_2017/main_guideline.m
+++ RF_2017/main_guideline.m
@@ -7,17 +7,18 @@ init;
 % Select dataset
 % we do bag-of-words technique to convert images to vectors (histogram of codewords)
 % Set 'showImg' in getData.m to 0 to stop displaying training and testing images and their feature vectors
+tic
 [data_train, data_test] = getData('Caltech_kmeans');
-close all;
+close all;%, classList_train
 
 
 
 % Set the random forest parameters ...
-param.num = 350;%10; % Number of trees
-param.depth = 9; % trees depth
-param.splitNum = 10;%3; % Number of split functions to try
+param.num = 100;%10; % Number of trees
+param.depth = 4; % trees depth
+param.splitNum = 5;%3; % Number of split functions to try
 param.split = 'IG'; % Currently support 'information gain' only
-param.split_func = 4;
+param.split_func = 1;
 
 % Train Random Forest ...
 [trees,ig_best] = growTrees(data_train,param);
@@ -25,6 +26,23 @@ param.split_func = 4;
 % Evaluate/Test Random Forest ...
 predictions = zeros(size(data_test,1),1);
 p_rf_sum = zeros(10,size(data_test,1));
+
+for n=1:size(data_test,1)
+leaves = testTrees(data_test(n,:),trees,param);
+% disp(leaves);
+% average the class distributions of leaf nodes of all trees
+p_rf = trees(1).prob(leaves,:);
+p_rf_sum(:,n) = sum(p_rf)/length(trees);
+[~,predictions(n)] = max(p_rf_sum(:,n));
+end
+tok
+
+% show accuracy and confusion matrix ...
+accuracy_test = sum(predictions==data_test(:,end))/size(data_test,1);
+conf = confusionmat(data_test(:,end), predictions);
+
+% str = input('1_4_5_1 ','s');
+% save([str,'.mat'],'param','trees','classList_train','accuracy_test','conf');
 
 for n=1:size(data_train,1)
 leaves = testTrees(data_train(n,:),trees,param);
@@ -37,19 +55,6 @@ end
 
 % show accuracy and confusion matrix ...
 accuracy_train = sum(predictions==data_train(:,end))/size(data_train,1);
-
-for n=1:size(data_test,1)
-leaves = testTrees(data_test(n,:),trees,param);
-% disp(leaves);
-% average the class distributions of leaf nodes of all trees
-p_rf = trees(1).prob(leaves,:);
-p_rf_sum(:,n) = sum(p_rf)/length(trees);
-[~,predictions(n)] = max(p_rf_sum(:,n));
-end
-
-% show accuracy and confusion matrix ...
-accuracy_test = sum(predictions==data_test(:,end))/size(data_test,1);
-
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % random forest codebook for Caltech101 image categorisation
