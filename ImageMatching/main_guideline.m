bc041c26fc84c9b2642d7468e79df45b3f1b9f54
unknown <ew913@eews506a-020.ic.ac.uk>
2017-04-03 17:19:23 +0100

    implemented fundamental matrix

--- ImageMatching/main_guideline.m
+++ ImageMatching/main_guideline.m
@@ -2,17 +2,34 @@
 
 clear all; close all; clc;
 
-[ ~ , C{1} ] = readppm('tsukuba/scene1.row3.col1.ppm');
-[ ~ , C{2} ] = readppm('tsukuba/scene1.row3.col2.ppm');
-[ ~ , C{3} ] = readppm('tsukuba/scene1.row3.col3.ppm');
-[ ~ , C{4} ] = readppm('tsukuba/scene1.row3.col4.ppm');
-[ ~ , C{5} ] = readppm('tsukuba/scene1.row3.col5.ppm');
-groundTruthImage = imread('tsukuba/truedisp.row3.col3.pgm');
-figure;imshow(C{1}(:,:,1));
-% figure;imshow(C{2}(:,:,1));
-% figure;imshow(C{3}(:,:,1));
-% figure;imshow(C{4}(:,:,1));
-figure;imshow(C{5}(:,:,1));
+imageset = 4;
+if imageset==0
+    % Tsukuba imageset
+    [ ~ , C{1} ] = readppm('tsukuba/scene1.row3.col1.ppm');
+    [ ~ , C{2} ] = readppm('tsukuba/scene1.row3.col2.ppm');
+    [ ~ , C{3} ] = readppm('tsukuba/scene1.row3.col3.ppm');
+    [ ~ , C{4} ] = readppm('tsukuba/scene1.row3.col4.ppm');
+    [ ~ , C{5} ] = readppm('tsukuba/scene1.row3.col5.ppm');
+    groundTruthImage = imread('tsukuba/truedisp.row3.col3.pgm');
+
+    image1 = C{1}(:,:,1);
+    image2 = C{5}(:,:,1);
+% HG imageset
+elseif imageset==1 % original size
+    image1 = rgb2gray(imread('HG_image/img1.JPG'));
+    image2 = rgb2gray(imread('HG_image/img2.JPG'));
+elseif imageset==2 % scaled size
+    image1 = imresize(rgb2gray(imread('HG_image/img2.JPG')),0.5);
+    image2 = imresize(rgb2gray(imread('HG_image/img3.JPG')),0.5);
+    
+% FD imageset
+elseif imageset==3 % original size
+    image1 = rgb2gray(imread('FD_image/img1.JPG'));
+    image2 = rgb2gray(imread('FD_image/img2.JPG'));
+elseif imageset==4 % scaled size
+    image1 = imresize(rgb2gray(imread('FD_image/img1.JPG')),0.5);
+    image2 = imresize(rgb2gray(imread('FD_image/img2.JPG')),0.5);
+end
 % figure;imshow(groundTruthImage);
 
 % Design 1
@@ -26,9 +43,6 @@ figure;imshow(C{5}(:,:,1));
 
 % https://github.com/gokhanozbulak/Harris-Detector.git
 
-image1 = C{1}(:,:,1);
-image2 = C{5}(:,:,1);
-
 % Harris Feature Extraction
 features1_raw = harris(image1, 0.04, 20000);
 features2_raw = harris(image2, 0.04, 20000);
@@ -39,9 +53,11 @@ desc_switch = 1;
 if desc_switch==0
     descriptor1 = sampledPatchDesc(image1, features1_raw);
     descriptor2 = sampledPatchDesc(image2, features2_raw);
+    sim_th = 1.5;
 elseif desc_switch==1
     descriptor1 = histDesc(image1, features1_raw);
     descriptor2 = histDesc(image2, features2_raw);
+    sim_th = 50;
 end
 % if size(descriptor1,1)>size(descriptor2,1)
 %     [matches, scores] = knnsearch(descriptor2,descriptor1); % descriptor1(1:length(corr_idx)) and descriptor2(corr_idx) are NN
@@ -59,18 +75,49 @@ features1 = [];features2 = [];
 feat_len = size(matches,2);
 idx = 1;
 for i = 1:feat_len
-    if scores(i)<40
+    if scores(i)<sim_th
         features1(:,idx) = features1_raw(1:2,matches(1,i));
         features2(:,idx) = features2_raw(1:2,matches(2,i));
         idx = idx + 1;
     end
 end
+
+% Messy fix: Correct the order of x and y coordinates
+features1 = [features1(2,:);features1(1,:)];
+features2 = [features2(2,:);features2(1,:)];
+
+% Show matched points (matched features)
+figure;
+showMatchedFeatures(image1,image2,features1',features2','montage','PlotOptions',{'ro','go','y--'});
+title('Putative point matches');
+
 % Calculate Homography Matrix using SVD
 H = homography_solve(features1, features2);
 
 projection = homography_transform(features1,H);
 
 HA = mean(sqrt(sum((projection - features2).^2)));
+
+% Calculate Fundamental Matrix
+[F, inliers] = estimateFundamentalMatrix(features1',features2','NumTrials',4000);
+
+% Epipolar Lines
+figure;
+subplot(121);
+imshow(image1);
+title('Features and Epipolar Lines in First Image'); hold on;
+plot(features1(1,inliers),features1(2,inliers),'go');
+epiLines = epipolarLine(F',features1(:,inliers)');
+points = lineToBorderPoints(epiLines,size(image1));
+line(points(:,[1,3])',points(:,[2,4])');
+
+subplot(122);
+imshow(image2);
+title('Features and Epipolar Lines in Second Image'); hold on;
+plot(features2(1,inliers),features2(2,inliers),'go');
+epiLines = epipolarLine(F,features2(:,inliers)');
+points = lineToBorderPoints(epiLines,size(image2));
+line(points(:,[1,3])',points(:,[2,4])');
 
 %% TB for homography_solve
 
@@ -88,15 +135,36 @@ clear all; close all; clc;
 
 init;
 
-[ ~ , C{1} ] = readppm('tsukuba/scene1.row3.col1.ppm');
-[ ~ , C{2} ] = readppm('tsukuba/scene1.row3.col2.ppm');
-[ ~ , C{3} ] = readppm('tsukuba/scene1.row3.col3.ppm');
-[ ~ , C{4} ] = readppm('tsukuba/scene1.row3.col4.ppm');
-[ ~ , C{5} ] = readppm('tsukuba/scene1.row3.col5.ppm');
-groundTruthImage = imread('tsukuba/truedisp.row3.col3.pgm');
+imageset = 0;
+if imageset==0
+    % Tsukuba imageset
+    [ ~ , C{1} ] = readppm('tsukuba/scene1.row3.col1.ppm');
+    [ ~ , C{2} ] = readppm('tsukuba/scene1.row3.col2.ppm');
+    [ ~ , C{3} ] = readppm('tsukuba/scene1.row3.col3.ppm');
+    [ ~ , C{4} ] = readppm('tsukuba/scene1.row3.col4.ppm');
+    [ ~ , C{5} ] = readppm('tsukuba/scene1.row3.col5.ppm');
+    groundTruthImage = imread('tsukuba/truedisp.row3.col3.pgm');
 
-image1 = C{1}(:,:,1);
-image2 = C{5}(:,:,1);
+    image1 = C{1}(:,:,1);
+    image2 = C{5}(:,:,1);
+% HG imageset
+elseif imageset==1 % original size
+    image1 = rgb2gray(imread('HG_image/img1.JPG'));
+    image2 = rgb2gray(imread('HG_image/img2.JPG'));
+elseif imageset==2 % scaled size
+    image1 = imresize(rgb2gray(imread('HG_image/img2.JPG')),0.5);
+    image2 = imresize(rgb2gray(imread('HG_image/img3.JPG')),0.5);
+    
+% FD imageset
+elseif imageset==3 % original size
+    image1 = rgb2gray(imread('FD_image/img1.JPG'));
+    image2 = rgb2gray(imread('FD_image/img2.JPG'));
+elseif imageset==4 % scaled size
+    image1 = imresize(rgb2gray(imread('FD_image/img1.JPG')),0.5);
+    image2 = imresize(rgb2gray(imread('FD_image/img2.JPG')),0.5);
+end
+
+
 
 % PHOW_Sizes = [4 8 10]; % Multi-resolution, these values determine the scale of each layer.
 % PHOW_Step = 8; % The lower the denser. Select from {2,4,8,16}
@@ -114,12 +182,17 @@ feat_len = size(matches,2);
 % features1 = zeros(2,feat_len); features2 = zeros(2,feat_len);
 idx = 1;
 for i = 1:feat_len
-    if scores(i)<10000
+    if scores(i)<8000
         features1(:,idx) = features1_raw(1:2,matches(1,i));
         features2(:,idx) = features2_raw(1:2,matches(2,i));
         idx = idx + 1;
     end
 end
+
+% Show matched points (matched features)
+figure;
+showMatchedFeatures(image1,image2,features1',features2','montage','PlotOptions',{'ro','go','y--'});
+title('Putative point matches');
 
 % Calculate Homography Matrix using SVD
 H = homography_solve(features1, features2);
@@ -127,3 +200,24 @@ H = homography_solve(features1, features2);
 projection = homography_transform(features1,H);
 
 HA = mean(sqrt(sum((projection - features2).^2)));
+
+% Calculate Fundamental Matrix
+[F, inliers] = estimateFundamentalMatrix(features1',features2','NumTrials',4000);
+
+% Epipolar Lines
+figure;
+subplot(121);
+imshow(image1);
+title('Features and Epipolar Lines in First Image'); hold on;
+plot(features1(1,inliers),features1(2,inliers),'go');
+epiLines = epipolarLine(F',features1(:,inliers)');
+points = lineToBorderPoints(epiLines,size(image1));
+line(points(:,[1,3])',points(:,[2,4])');
+
+subplot(122);
+imshow(image2);
+title('Features and Epipolar Lines in Second Image'); hold on;
+plot(features2(1,inliers),features2(2,inliers),'go');
+epiLines = epipolarLine(F,features2(:,inliers)');
+points = lineToBorderPoints(epiLines,size(image2));
+line(points(:,[1,3])',points(:,[2,4])');
